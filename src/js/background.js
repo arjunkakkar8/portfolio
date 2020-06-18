3e91626df90022c1df40220e7d81f5930ed1cb6f
arjunkakkar8 <arjunkakkar8@gmail.com>
2020-06-18 06:18:03 -0700

    Removed stats and added one description

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,4 @@
 import * as THREE from "three";
-import Stats from "stats.js";
-
-let stats = new Stats();
-stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild( stats.dom );
 
 let container;
 let camera, scene, renderer;
@@ -80,9 +75,7 @@ function onWindowResize(event) {
 }
 
 function animate() {
-  stats.begin();
   render();
-  stats.end();
   requestAnimationFrame(animate);
 }
 
